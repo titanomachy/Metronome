66b2f1c565e6100b2db7096598e597aa3fd29f70
titanomachy <1170510+titanomachy@users.noreply.github.com>
2026-07-10 17:06:34 +0200

    Fix LCOV 2.1 coverage report generation

--- code_coverage.sh
+++ code_coverage.sh
@@ -6,5 +6,5 @@ cd "$repo_root"
 
 lcov --ignore-errors inconsistent,unused,mismatch,missing,source,empty,gcov,range --filter range --capture --directory nimcache --output-file coverage.info
 lcov --ignore-errors inconsistent,unused,mismatch,missing,source,empty,gcov --extract coverage.info "$repo_root/src/*" --output-file coverage.info
-genhtml --ignore-errors range --filter missing coverage.info --output-directory coverage_html
+genhtml --ignore-errors range coverage.info --output-directory coverage_html
 scripts/coverage_badge.sh coverage.info docs/coverage.svg
